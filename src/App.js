d1d665ea4d369f11fc59410227f3e8079b893d95
jeevankishore-v <jeavankishore2006@gmail.com>
2025-10-27 11:54:27 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SecureMedicalDataSharing from './components/SecureMedicalDataSharing';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SecureMedicalDataSharing />
+    </div>
+  );
+}
+
+export default App;
